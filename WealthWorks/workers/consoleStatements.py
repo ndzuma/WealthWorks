b48a35c2f92d7a47da3270dba018397791a672ff
whitefaalconn <ndzumaxx@gmail.com>
2024-03-13 23:27:42 +0000

    Added consoleStatements.py to display things on the console

---
+++ WealthWorks/workers/consoleStatements.py
@@ -0,0 +1,23 @@
+def start(service: str = "WealthWorks"):
+    """
+    Print a message to the console when a service starts
+    :param service: The name of the service starting
+    """
+    print("Starting", service)
+
+
+def completed(service: str = "WealthWorks"):
+    """
+    Print a message to the console when a service completes
+    :param service: The name of the service being completed
+    """
+    print(service, "completed.")
+
+
+def message(service: str, text: str):
+    """
+    Print a message to the console
+    :param service: The name of the service displaying a message
+    :param text: The message to be displayed
+    """
+    print(f"{service}: {text}")
