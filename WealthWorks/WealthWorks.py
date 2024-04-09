cc1feefe2e7a131fa79c62724b5cc0f956ba61d3
whitefaalconn <ndzumaxx@gmail.com>
2024-04-09 05:07:28 +0100

    hid tracking details

--- WealthWorks/WealthWorks.py
+++ WealthWorks/WealthWorks.py
@@ -1,12 +1,14 @@
 """Welcome to WealthWorks!"""
 import reflex as rx
 
+# Importing pages
 from WealthWorks.pages.debtRepayment import debt_repayment
 from WealthWorks.pages.goodToKnow import good_to_know
 from WealthWorks.pages.budgetPlanner import planner
 from WealthWorks.pages.news import news
 from WealthWorks.pages.page404 import page404
-
+from dotenv import load_dotenv
+import os
 
 # All pages
 @rx.page(
@@ -49,17 +51,21 @@ def not_found() -> rx.Component:
     return page404()
 
 
+load_dotenv()
+umami_url = os.environ.get("UMAMI_URL")
+website_id = os.environ.get("WEBSITE_ID")
+
 app = rx.App(
     head_components=[
         rx.script(
-            """
-            (function() {
+            f"""
+            (function() {{
                 var script = document.createElement('script');
                 script.async = true;
-                script.src = "https://umami.ndzuma.pro/script.js";
-                script.setAttribute('data-website-id', 'b9493033-fdcb-451d-addc-7c8d17e6e64f');
+                script.src = "{umami_url}";
+                script.setAttribute('data-website-id', '{website_id}');
                 document.head.appendChild(script);
-            }) ();
+            }}) ();
             """
         ),
     ],
